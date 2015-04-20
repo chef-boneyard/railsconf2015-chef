d44d6c610342a94384a603739439a95f6d13400d
nathenharvey <nharvey@chef.io>
2015-04-20 04:53:53 -0400

    test for the postgresql package

--- chef-repo/cookbooks/widget_world_postgres/test/integration/default/serverspec/default_spec.rb
+++ chef-repo/cookbooks/widget_world_postgres/test/integration/default/serverspec/default_spec.rb
@@ -5,8 +5,8 @@ describe 'widget_world_postgres::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
   
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  it 'installs postgres' do
+    expect(package('postgresql')).to be_installed
   end
 
 end
