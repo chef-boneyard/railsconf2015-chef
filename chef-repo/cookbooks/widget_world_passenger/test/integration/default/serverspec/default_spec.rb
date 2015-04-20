dc17dc04af390b7e3ca724c2652cf0b3a9b2e572
nathenharvey <nharvey@chef.io>
2015-04-19 22:25:40 -0400

    add a missing slash in the regex definition

--- chef-repo/cookbooks/widget_world_passenger/test/integration/default/serverspec/default_spec.rb
+++ chef-repo/cookbooks/widget_world_passenger/test/integration/default/serverspec/default_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'widget_world_passenger::default' do
 
   it 'installs passenger version 5.0.6' do
-    expect(command('passenger -v').stdout).to match(/5\.0\.6)
+    expect(command('passenger -v').stdout).to match(/5\.0\.6/)
   end
 
 end
