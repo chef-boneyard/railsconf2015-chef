1fe7c8e488fcecf582aa0abceb0242527c8473a1
nathenharvey <nharvey@chef.io>
2015-04-19 22:01:12 -0400

    A widget_world_apache cookbook
    * cd chef-repo/cookbooks
    * chef generate cookbook widget_world_apache

---
+++ chef-repo/cookbooks/widget_world_apache/README.md
@@ -0,0 +1,4 @@
+# widget_world_apache
+
+TODO: Enter the cookbook description here.
+
---
+++ chef-repo/cookbooks/widget_world_apache/metadata.rb
@@ -0,0 +1,8 @@
+name             'widget_world_apache'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures widget_world_apache'
+long_description 'Installs/Configures widget_world_apache'
+version          '0.1.0'
+
