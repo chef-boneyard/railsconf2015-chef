bef5575235628bcf1ed3d57736127fd8b9af9832
nathenharvey <nharvey@chef.io>
2015-04-19 21:55:46 -0400

    Fix the serverspec test

--- chef-repo/cookbooks/widget_world_ruby/test/integration/default/serverspec/default_spec.rb
+++ chef-repo/cookbooks/widget_world_ruby/test/integration/default/serverspec/default_spec.rb
@@ -6,7 +6,7 @@ describe 'widget_world_ruby::default' do
   # http://burtlo.github.io/serverspec.github.io/
   
   it 'installs ruby 2.2.2' do
-    expect (command("ruby -v").stdout).to match /^ruby 2.2.2/
+    expect(command("ruby -v").stdout).to match /^ruby 2.2.2/
   end
 
 end
