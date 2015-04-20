a751ac7b1924b647dd220a26bb32f35e6a36d64b
nathenharvey <nharvey@chef.io>
2015-04-20 07:10:21 -0400

    The /etc/postgresql/9.1/main/pg_hba.conf should be managed by chef

--- chef-repo/cookbooks/widget_world_postgres/recipes/default.rb
+++ chef-repo/cookbooks/widget_world_postgres/recipes/default.rb
@@ -9,3 +9,16 @@ include_recipe 'apt::default'
 package 'postgresql'
 package 'postgresql-contrib'
 package 'libpq-dev'
+
+execute 'reload postges' do
+  command 'pg_ctl reload'
+  action :nothing
+end
+
+template '/etc/postgresql/9.1/main/pg_hba.conf' do
+  source 'pg_hba.conf.erb'
+  owner 'postgres'
+  group 'postgres'
+  mode '0640'
+  notifies :run, 'execute[reload postges]', :immediately
+end
