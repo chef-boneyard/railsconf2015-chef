ffd67b58686b667c5e9db90b63df7cfce034988b
nathenharvey <nharvey@chef.io>
2015-04-19 22:30:18 -0400

    Add an apt_repository for passenger

--- chef-repo/cookbooks/widget_world_passenger/recipes/default.rb
+++ chef-repo/cookbooks/widget_world_passenger/recipes/default.rb
@@ -3,3 +3,9 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+apt_repository 'passenger' do
+  uri 'https://oss-binaries.phusionpassenger.com/apt/passenger'
+  components ['precise', 'main']
+  keyserver 'keyserver.ubuntu.com'
+  key '561F9B9CAC40B2F7'
+end
