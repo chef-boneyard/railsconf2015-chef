a9222431242e09b7a9b77527d30f096c1df196be
nathenharvey <nharvey@chef.io>
2015-04-20 16:37:52 -0400

    This cookbook depends on widget_world_postgres

--- chef-repo/cookbooks/widget_world_application/metadata.rb
+++ chef-repo/cookbooks/widget_world_application/metadata.rb
@@ -5,4 +5,4 @@ license          'all_rights'
 description      'Installs/Configures widget_world_application'
 long_description 'Installs/Configures widget_world_application'
 version          '0.1.0'
-
+depends          'widget_world_postgres'
