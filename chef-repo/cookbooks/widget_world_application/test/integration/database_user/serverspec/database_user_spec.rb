1268b122e49a82d120061387b00cfa17cbb8722d
nathenharvey <nharvey@chef.io>
2015-04-20 13:48:02 -0400

    add test for custom database user

--- chef-repo/cookbooks/widget_world_application/test/integration/database_user/serverspec/database_user_spec.rb
+++ chef-repo/cookbooks/widget_world_application/test/integration/database_user/serverspec/database_user_spec.rb
@@ -2,11 +2,8 @@ require 'spec_helper'
 
 describe 'widget_world_application::database_user' do
 
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  it 'creates the database user' do
+    expect(command('sudo -u postgres psql -tAc \"SELECT 1 FROM pg_roles WHERE rolname=\'widgetworld\'\" | wc -l`.chomp == "1"').stdout).to match \1\
   end
 
 end
