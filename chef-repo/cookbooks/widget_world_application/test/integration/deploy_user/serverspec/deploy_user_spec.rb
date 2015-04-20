5b50166e85d1008bdd938580bcacb24ef604dcec
nathenharvey <nharvey@chef.io>
2015-04-20 11:00:03 -0400

    Test for the deploy user and that user's uid

--- chef-repo/cookbooks/widget_world_application/test/integration/deploy_user/serverspec/deploy_user_spec.rb
+++ chef-repo/cookbooks/widget_world_application/test/integration/deploy_user/serverspec/deploy_user_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe 'widget_world_application::deploy_user' do
 
   it 'creates the deploy user' do
-    expect('deploy').to exist
+    expect(user 'deploy').to exist
   end
 
   it 'gives the deploy user a uid of 500' do
-    expect('deploy').to have_uid(500)
+    expect(user 'deploy').to have_uid(500)
   end
 
 end
