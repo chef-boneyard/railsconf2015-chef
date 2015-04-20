e7de03411e2b85c721e45a43272c07d560633ff3
nathenharvey <nharvey@chef.io>
2015-04-19 22:07:37 -0400

    Test an http request against localhost

--- chef-repo/cookbooks/widget_world_apache/test/integration/default/serverspec/default_spec.rb
+++ chef-repo/cookbooks/widget_world_apache/test/integration/default/serverspec/default_spec.rb
@@ -1,12 +1,9 @@
 require 'spec_helper'
 
 describe 'widget_world_apache::default' do
-
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
   
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  it 'responds to http requests' do
+    expect(command('curl http://localhost').exit_status).to eq 0
   end
 
 end
