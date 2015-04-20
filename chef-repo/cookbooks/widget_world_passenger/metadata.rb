58df9a9cd9faaa0652efdb39a42bd4f5e1303b58
nathenharvey <nharvey@chef.io>
2015-04-19 22:28:42 -0400

    Add a dependency on apt
    This makes the apt_repository resource available in
    
    * recipes
    * ChefSpec

--- chef-repo/cookbooks/widget_world_passenger/metadata.rb
+++ chef-repo/cookbooks/widget_world_passenger/metadata.rb
@@ -5,4 +5,4 @@ license          'all_rights'
 description      'Installs/Configures widget_world_passenger'
 long_description 'Installs/Configures widget_world_passenger'
 version          '0.1.0'
-
+depends          'apt'
