ce4c60ff948657636c70a592081b43016e4c48c4
nathenharvey <nharvey@chef.io>
2015-04-20 16:23:31 -0400

    stub out some commands in the unit test

--- chef-repo/cookbooks/widget_world_application/spec/unit/recipes/database_user_spec.rb
+++ chef-repo/cookbooks/widget_world_application/spec/unit/recipes/database_user_spec.rb
@@ -14,14 +14,15 @@ describe 'widget_world_application::database_user' do
       runner = ChefSpec::ServerRunner.new
       runner.converge(described_recipe)
     end
-
+    
+    before do
+      stub_command("psql -tAc \"SELECT 1 FROM pg_roles WHERE rolname = 'widgetworld'\" | grep 1").and_return(1)
+      stub_command("psql -c \"create user widgetworld with password 'super_secret';\"; psql -c \"alter user widgetworld CREATEDB;\"").and_return(0)
+    end
+  
     it 'converges successfully' do
       chef_run # This should not raise an error
     end
     
-    it 'creates the database user' do
-      expect(chef_run).to run_execute('create new postgres user')
-    end
-
   end
 end
