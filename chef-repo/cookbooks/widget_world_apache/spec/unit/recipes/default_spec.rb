87781feea43f54d2ec8a28218fb9f18de3d173dd
nathenharvey <nharvey@chef.io>
2015-04-19 22:14:00 -0400

    test for the apache2 service

--- chef-repo/cookbooks/widget_world_apache/spec/unit/recipes/default_spec.rb
+++ chef-repo/cookbooks/widget_world_apache/spec/unit/recipes/default_spec.rb
@@ -22,6 +22,10 @@ describe 'widget_world_apache::default' do
     it 'installs the apache package' do
       expect(chef_run).to install_package('apache2')
     end
+    
+    it 'starts the apache service' do
+      expect(chef_run).to start_service('apache2')
+    end
 
   end
 end
