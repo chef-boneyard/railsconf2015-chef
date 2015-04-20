166667f7bf83a8fd631636dfe48c825b20ecb11d
nathenharvey <nharvey@chef.io>
2015-04-20 15:47:46 -0400

    fix the database user test

--- chef-repo/cookbooks/widget_world_application/test/integration/database_user/serverspec/database_user_spec.rb
+++ chef-repo/cookbooks/widget_world_application/test/integration/database_user/serverspec/database_user_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'widget_world_application::database_user' do
 
   it 'creates the database user' do
-    expect(command('sudo -u postgres psql -tAc \"SELECT 1 FROM pg_roles WHERE rolname=\'widgetworld\'\" | wc -l`.chomp == "1"').stdout).to match \1\
+    expect(command('su  - postgres  -c "psql -tAc \"SELECT 1 FROM pg_roles WHERE rolname=\'widgetworld\'\""').stdout.chomp!).to eq "1"
   end
 
 end
