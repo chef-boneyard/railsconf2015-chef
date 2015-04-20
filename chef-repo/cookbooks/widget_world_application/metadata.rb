afc0b48b43a1f80aac66873c3a10eb59b653b6b1
nathenharvey <nharvey@chef.io>
2015-04-20 07:24:16 -0400

    a scaffold for the widget_world_application cookbook
    * cd chef-repo/cookbooks
    * chef generate cookbook widget_world_application

---
+++ chef-repo/cookbooks/widget_world_application/metadata.rb
@@ -0,0 +1,8 @@
+name             'widget_world_application'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures widget_world_application'
+long_description 'Installs/Configures widget_world_application'
+version          '0.1.0'
+
